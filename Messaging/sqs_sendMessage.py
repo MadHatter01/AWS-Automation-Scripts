ccc808affb8aaefd8e60c92482629803b6a9dce7
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-04-17 07:56:41 -0700

    SQS Send Message

---
+++ Messaging/sqs_sendMessage.py
@@ -0,0 +1,18 @@
+import boto3
+
+def send_message(sqs_client, queue_url, message):
+    response = sqs_client.send_message(
+        QueueUrl = queue_url,
+        MessageBody = message,
+        DelaySeconds=100
+    )
+    print(f"Sent message: {message}, MessageId: {response['MessageId']}")
+
+def main():
+    sqs_client = boto3.client('sqs')
+    queue_url = "[queue-url]"
+    message = "Hello world"
+    send_message(sqs_client, queue_url, message)
+
+if __name__ == "__main__":
+    main()
