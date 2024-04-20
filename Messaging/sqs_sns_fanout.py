1adfaf21a43116804c2bc8f0925654b1faed5e4c
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-04-20 10:56:54 -0700

    Fanout with SNS - SQS and Email

---
+++ Messaging/sqs_sns_fanout.py
@@ -0,0 +1,54 @@
+import boto3
+import datetime
+import json
+
+# subscribe an existing sqs to an sns topic
+
+def create_sqs_subscription(sns_client, topic_arn, queue_arn):
+    sqs_subscription = sns_client.subscribe(
+        TopicArn = topic_arn, 
+        Protocol = 'sqs',
+        Endpoint = queue_arn
+    )
+    print(f"SQS Subscription successful. Subscription Arn: {sqs_subscription['SubscriptionArn']}")
+
+# you have to explicitly confirm email subscription to receive emails. Use an existing subscription
+def create_email_subscription(sns_client, topic_arn, email_address):
+    email_subscription = sns_client.subscribe(
+        TopicArn = topic_arn, 
+        Protocol = 'email',
+        Endpoint = email_address
+    )
+    print(f"Email Subscription successful. Subscription Arn: {email_subscription['SubscriptionArn']}")
+
+
+def simulate_activity(sns_client, topic_arn):
+    user_activity = {
+        'event_type':'Order Placement',
+        'user_id':'Jane123',
+        'timestamp':datetime.datetime.utcnow().isoformat(),
+        'details':'Order for Shoes Model X placed'
+
+    }
+    response = sns_client.publish(
+        TopicArn = topic_arn, 
+        Message = json.dumps(user_activity)
+    )
+    print(f"Published Message to subscribers (SQS, Email)", response)
+
+
+def main():
+    sns_client = boto3.client('sns')
+    queue_arn = "[sqs_queue_arn]"
+    email_address="[email-id]"
+    topic_arn = "[sns_topic_arn]"
+    create_sqs_subscription(sns_client, topic_arn, queue_arn)
+    create_email_subscription(sns_client, topic_arn, email_address)
+
+    simulate_activity(sns_client, topic_arn)
+
+
+if __name__=="__main__":
+    main()
+
+
